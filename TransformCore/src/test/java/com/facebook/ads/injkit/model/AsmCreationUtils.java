40bcac077a9d6e8cece57663afb82514968b24ff
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-03-06 07:09:32 -0800

    fbandroid/first-party/java/androidsdk/ads/InjKit/TransformCore/src/test/java/com/facebook/ads/injkit/model/AsmCreationUtils.java
    Reviewed By: jocelynluizzi13
    
    Differential Revision: D70691742
    
    fbshipit-source-id: 6529494a1644dc9316a52f83031679cb89be5e2b

--- TransformCore/src/test/java/com/facebook/ads/injkit/model/AsmCreationUtils.java
+++ TransformCore/src/test/java/com/facebook/ads/injkit/model/AsmCreationUtils.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
@@ -150,7 +151,7 @@ class AsmCreationUtils {
     return mn;
   }
 
-  static AnnotationNode makeAnnotation(String desc, Object... properties) {
+  static AnnotationNode makeAnnotation(String desc, @Nullable Object... properties) {
     AnnotationNode node = new AnnotationNode(desc);
     if (properties != null && properties.length > 0) {
       node.values = Arrays.asList(properties);
