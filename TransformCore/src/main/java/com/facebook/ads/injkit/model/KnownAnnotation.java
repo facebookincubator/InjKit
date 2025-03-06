03bbdba99f9d4b826da88479f8385bdfc2075059
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-03-06 08:00:39 -0800

    fbandroid/first-party/java/androidsdk/ads/InjKit/TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownAnnotation.java
    Reviewed By: jocelynluizzi13
    
    Differential Revision: D70691017
    
    fbshipit-source-id: c7ca75413f679e2fc0cb2cdfba2fb0481cfcfda5

--- TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownAnnotation.java
+++ TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownAnnotation.java
@@ -7,18 +7,22 @@
 
 package com.facebook.ads.injkit.model;
 
+import com.facebook.infer.annotation.Nullsafe;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.annotation.Nullable;
 import org.objectweb.asm.tree.AnnotationNode;
 
+@Nullsafe(Nullsafe.Mode.LOCAL)
 class KnownAnnotation {
   private final String desc;
   private final Map<String, Object> values;
 
   private KnownAnnotation(AnnotationNode node) {
+    // NULLSAFE_FIXME[Not Vetted Third-Party]
     desc = node.desc;
     values = new HashMap<>();
     for (int i = 0; node.values != null && i < (node.values.size() - 1); i += 2) {
@@ -31,10 +35,12 @@ class KnownAnnotation {
   }
 
   Object getValue(String key) {
+    // NULLSAFE_FIXME[Return Not Nullable]
     return values.get(key);
   }
 
-  static Set<KnownAnnotation> from(List<AnnotationNode> visible, List<AnnotationNode> invisible) {
+  static Set<KnownAnnotation> from(
+      @Nullable List<AnnotationNode> visible, @Nullable List<AnnotationNode> invisible) {
     Set<KnownAnnotation> annotations = new HashSet<>();
     if (visible != null) {
       for (AnnotationNode annotationNode : visible) {
