09f94a470aa21f637b78f2705991a7e583012188
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-03-14 09:14:22 -0700

    fbandroid/first-party/java/androidsdk/ads/InjKit/TransformCore/src/main/java/com/facebook/ads/injkit/InvalidMethodDefinitionException.java
    Differential Revision: D71189678
    
    fbshipit-source-id: bb155d460e9f38ce7fecd4856f341284face9cb8

--- TransformCore/src/main/java/com/facebook/ads/injkit/InvalidMethodDefinitionException.java
+++ TransformCore/src/main/java/com/facebook/ads/injkit/InvalidMethodDefinitionException.java
@@ -7,6 +7,9 @@
 
 package com.facebook.ads.injkit;
 
+import com.facebook.infer.annotation.Nullsafe;
+
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class InvalidMethodDefinitionException extends AnnotationProcessingException {
   public InvalidMethodDefinitionException(String description) {
     super(description);
