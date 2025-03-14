cafaf703ef8df31f67b61eec034b9a2fb002d5f3
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-03-14 09:13:29 -0700

    fbandroid/first-party/java/androidsdk/ads/InjKit/TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownMethod.java
    Differential Revision: D71191401
    
    fbshipit-source-id: fed861bd84251588c39ebab0425c5d05b67ec525

--- TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownMethod.java
+++ TransformCore/src/main/java/com/facebook/ads/injkit/model/KnownMethod.java
@@ -7,11 +7,13 @@
 
 package com.facebook.ads.injkit.model;
 
+import com.facebook.infer.annotation.Nullsafe;
 import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.objectweb.asm.tree.MethodNode;
 
+@Nullsafe(Nullsafe.Mode.LOCAL)
 class KnownMethod {
   private final String name;
   private final String desc;
@@ -20,11 +22,14 @@ class KnownMethod {
   private final Object annotationDefaultValue;
 
   KnownMethod(MethodNode methodNode) {
+    // NULLSAFE_FIXME[Not Vetted Third-Party]
     name = methodNode.name;
+    // NULLSAFE_FIXME[Not Vetted Third-Party]
     desc = methodNode.desc;
     access = methodNode.access;
     annotations =
         KnownAnnotation.from(methodNode.visibleAnnotations, methodNode.invisibleAnnotations);
+    // NULLSAFE_FIXME[Not Vetted Third-Party]
     annotationDefaultValue = methodNode.annotationDefault;
   }
 
